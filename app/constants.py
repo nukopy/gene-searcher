f827a7afba2e94d8dd640d80b9459f4203331c90
nukopy <nukopy@gmail.com>
2024-01-27 23:41:43 +0900

    feat: add data sources

--- app/constants.py
+++ app/constants.py
@@ -1,5 +1,8 @@
 DATA_SOURCE_NAME_ENSEMBL = "Ensembl"
 DATA_SOURCE_NAME_HUMAN_PROTEIN_ATLAS = "The Human Protein Atlas"
+DATA_SOURCE_NAME_DICE = "DICE"
+DATA_SOURCE_NAME_BIOGPS = "BioGPS"
+DATA_SOURCE_NAME_BENCHSCI = "BenchSci"
 
 DATA_SOURCES: list[dict[str, str]] = [
     {
@@ -11,5 +14,20 @@ DATA_SOURCES: list[dict[str, str]] = [
         "name": DATA_SOURCE_NAME_HUMAN_PROTEIN_ATLAS,
         "version": "23.0",
         "url": "https://www.proteinatlas.org",
+    },
+    {
+        "name": DATA_SOURCE_NAME_DICE,
+        "version": "TODO",  # TODO: バージョン情報
+        "url": "https://dice-database.org",
+    },
+    {
+        "name": DATA_SOURCE_NAME_BIOGPS,
+        "version": "TODO",  # TODO: バージョン情報
+        "url": "https://biogps.org",
+    },
+    {
+        "name": DATA_SOURCE_NAME_BENCHSCI,
+        "version": "TODO",  # TODO: バージョン情報
+        "url": "https://www.benchsci.com",
     },
 ]
