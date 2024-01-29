f3fd6d8b489bb291749a2903da3d9058dacbd8fd
nukopy <nukopy@gmail.com>
2024-01-29 23:57:44 +0900

    feat: add error handling of fetch

--- app/client.py
+++ app/client.py
@@ -10,9 +10,31 @@ async def fetch(
     url: str,
     params: dict = None,
     headers: dict = None,
+    timeout_seconds: int = 30,
 ) -> dict:
-    res = await session.get(url, params=params, headers=headers)
-    logger.info(f"fetch data from {res.url}")
-    data = await res.json()
+    # setup HTTP headers
+    default_headers = {
+        "Content-Type": "application/json",
+    }
+    if headers is None:
+        headers = default_headers
+    else:
+        headers = default_headers.update(headers)
 
-    return data
+    try:
+        res = await session.get(
+            url, params=params, headers=headers, timeout=timeout_seconds
+        )
+        res.raise_for_status()  # raise error if status is not 200-299
+        logger.info(f"fetch data successfully from {res.url}")
+        data = await res.json()
+
+        return data
+    except aiohttp.ClientError as e:
+        logger.error(
+            f"failed to fetch data from {url} with status {res.status} due to client error: {e}"
+        )
+        raise e
+    except Exception as e:
+        logger.error(f"failed to fetch data from {url} due to unexpected error: {e}")
+        raise e
