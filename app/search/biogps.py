bf166ff87ac2095ef0c20c2255443c6d6297fdc3
nukopy <nukopy@gmail.com>
2024-02-02 04:27:50 +0900

    feat: deal with custom error type on each search function

--- app/search/biogps.py
+++ app/search/biogps.py
@@ -2,6 +2,7 @@ import aiohttp
 
 # from app.client import fetch
 from app.constants import DATA_SOURCE_NAME_BENCHSCI
+from app.errors import FetchClientError, FetchClientResponseError, FetchUnexpectedError
 from app.logger import create_logger
 
 logger = create_logger(__name__)
@@ -18,13 +19,17 @@ async def search_biogps(session: aiohttp.ClientSession, query: str) -> (str, dic
         res = {}
 
         return (DATA_SOURCE_NAME_BENCHSCI, res)
-    except aiohttp.ClientError as e:
-        # msg = f"Error on search_biogps: failed to fetch data from {api_url} with status {res.status} due to client error"
+    except FetchClientResponseError as e:
+        msg = f"Error on search_biogps: failed to fetch data from {api_url} due to response error"
+        logger.error(msg)
+
+        raise Exception(f"{msg}: {e}") from e
+    except FetchClientError as e:
         msg = f"Error on search_biogps: failed to fetch data from {api_url} due to client error"
         logger.error(msg)
 
         raise Exception(f"{msg}: {e}") from e
-    except Exception as e:
+    except FetchUnexpectedError as e:
         msg = f"Error on search_biogps: failed to fetch data from {api_url} due to unexpected error"
         logger.error(msg)
 
