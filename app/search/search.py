294940a085d40b296a7f14a5bb0184c5b2625531
nukopy <nukopy@gmail.com>
2024-02-03 03:23:57 +0900

    refactor: add type hints

--- app/search/search.py
+++ app/search/search.py
@@ -19,17 +19,20 @@ from app.search.human_protein_atlas import search_hpa
 
 logger = create_logger(__name__)
 
-ResultType = Union[Tuple[str, dict], Exception]
+HpaResultType = dict
+DiceResultType = bytes
+FetchResultType = Tuple[str, Union[HpaResultType, DiceResultType]]
+TaskResultType = Union[FetchResultType, Exception]
+DataType = Union[Union[HpaResultType, DiceResultType], Exception]
 
 
-async def _search(query: str) -> (dict, float):
+async def _search(query: str) -> Tuple[dict, float]:
     logger.info(f"start searching by query '{query}'...")
     start = time.time()
 
     # start async tasks
-    global session
-    tasks = []
-    results: list[(str, dict)] = []
+    tasks: list = []
+    results: list[TaskResultType] = []
     async with aiohttp.ClientSession() as session:
         # FIXME: asyncio.TaskGroup のエラーハンドリング必要？多分必要なさそうだけど
         # ref: https://gihyo.jp/article/2022/10/monthly-python-2210
@@ -47,10 +50,10 @@ async def _search(query: str) -> (dict, float):
         tasks.append(search_benchsci(session, query))
 
         # await all tasks
-        results: list[ResultType] = await asyncio.gather(*tasks, return_exceptions=True)
+        results = await asyncio.gather(*tasks, return_exceptions=True)
 
     # extract each result
-    data: dict[str, Union[dict, Exception]] = {}
+    data: dict[str, DataType] = {}
 
     # caution: result の結果の順序は tasks.append した順番になるので注意
     # FIXME: もうちょっときれいに書けないかな。raise しないで Exception を戻り値にするとか
@@ -73,14 +76,12 @@ async def _search(query: str) -> (dict, float):
 
     end = time.time()
     diff = end - start
-
-    logger.info(f"data: {data}")
     logger.info(f"end searching by query '{query}' (takes {diff:.4f} sec)")
 
     return data, end - start
 
 
 @st.cache_data
-def search(query: str) -> (dict, float):
+def search(query: str) -> Tuple[dict, float]:
     """sync wrapper for caching API responses on streamlit"""
     return asyncio.run(_search(query))
